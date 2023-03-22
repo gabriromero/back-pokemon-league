5521b84c2e15e3084f71950a896d84aef3953fdb
GabriMac <gabri1998gabri@gmail.com>
2023-03-22 22:18:19 +0100

    removed create_app function

--- app.py
+++ app.py
@@ -14,42 +14,39 @@ from db import db
 from resources.player import blp as PlayerBlueprint
 from resources.match import blp as MatchBlueprint
 
-def create_app(db_url=None):
-    app = Flask(__name__)
-    load_dotenv()
-    CORS(app)
+app = Flask(__name__)
+load_dotenv()
+CORS(app)
 
-    app.config['API_SPEC_OPTIONS'] = {
-        'security':[{"bearerAuth": []}],
-        'components':{
-            "securitySchemes":
-                {
-                    "bearerAuth": {
-                        "type":"http",
-                        "scheme": "bearer",
-                        "bearerFormat": "JWT"
-                    }
+app.config['API_SPEC_OPTIONS'] = {
+    'security':[{"bearerAuth": []}],
+    'components':{
+        "securitySchemes":
+            {
+                "bearerAuth": {
+                    "type":"http",
+                    "scheme": "bearer",
+                    "bearerFormat": "JWT"
                 }
-        }
+            }
     }
+}
 
-    app.config["PROPAGATE_EXCEPTIONS"] = True
-    app.config["API_TITLE"] = "pokemon-league API"
-    app.config["API_VERSION"] = "v1"
-    app.config["OPENAPI_VERSION"] = "3.0.3"
-    app.config["OPENAPI_URL_PREFIX"] = "/"
-    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL","sqlite:///data.db")
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.init_app(app)
-    migrate = Migrate(app, db)
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["API_TITLE"] = "pokemon-league API"
+app.config["API_VERSION"] = "v1"
+app.config["OPENAPI_VERSION"] = "3.0.3"
+app.config["OPENAPI_URL_PREFIX"] = "/"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL","sqlite:///data.db")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+migrate = Migrate(app, db)
 
-    api = Api(app)
+api = Api(app)
 
-    app.config["JWT_SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["JWT_SECRET_KEY"] = os.getenv("SECRET_KEY")
 
-    api.register_blueprint(PlayerBlueprint)
-    api.register_blueprint(MatchBlueprint)
-
-    return app
+api.register_blueprint(PlayerBlueprint)
+api.register_blueprint(MatchBlueprint)
