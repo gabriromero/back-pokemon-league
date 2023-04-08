1892c374bdd3d6249db29368340a023f0b3e5006
GabriMac <gabri1998gabri@gmail.com>
2023-04-08 14:31:55 +0200

    username not key sensitive

--- resources/player.py
+++ resources/player.py
@@ -136,10 +136,10 @@ class Login(MethodView):
     @blp.arguments(LoginPlayerSchema)
     def post(self, user_data):
         player = PlayerModel.query.filter(
-            PlayerModel.username.lower() == user_data["username"].lower()
+            or_(PlayerModel.username.ilike(user_data["username"]), PlayerModel.username.ilike(user_data["username"].lower()))
         ).first()
 
-        if player and player.password.lower() == user_data["password"].lower():
+        if player and player.password == user_data["password"]:
             access_token = create_access_token(identity=player.id)
             return {"access_token" : access_token}
         
