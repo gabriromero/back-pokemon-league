50fc9645e2e5fe0575b4ed0fd260108b8d9356c6
GabriMac <gabri1998gabri@gmail.com>
2023-04-17 22:48:30 +0200

    cambiado nombre skin por defecto

--- models/player.py
+++ models/player.py
@@ -6,7 +6,7 @@ class PlayerModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
-    profile_pic = db.Column(db.String(255), default='trainerPixel')
+    profile_pic = db.Column(db.String(255), default='trainer-1')
     matches_played = db.Column(db.Integer, default=0)
     matches_won = db.Column(db.Integer, default=0)
     matches_won_frozen = db.Column(db.Integer, default=0)
